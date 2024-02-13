1db31284cfa9d94bf334290e1144af00608b7641
Nitin Chaudhary <nc1575612@gmail.com>
2024-02-13 14:36:51 +0530

    nav-bar added

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
 var typed = new Typed('#element', {
     strings: ['<i>C++</i> Developer', '&amp; Ful Stack Dev'],
     typeSpeed: 50,
