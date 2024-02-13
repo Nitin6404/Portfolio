459c0323f9c4acaa571643fb01a4a1f62576b9ed
Nitin Chaudhary <nc1575612@gmail.com>
2024-02-13 22:56:52 +0530

    add responsive nav bar

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-
-var typed = new Typed('#element', {
-    strings: ['<i>A Passionate Full Stack Dev</i>', '<i>&amp; Open Source Contributor</i>'],
-    typeSpeed: 50,
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed('#element', {
+      strings: ['<i>A Passionate Full Stack Dev</i>', '<i>&amp; Open Source Contributor</i>'],
+      typeSpeed: 50,
   });
+
+  var mobileMenuBtn = document.getElementById("mobile-menu-btn");
+  var navLinks = document.querySelector(".nav-links");
+
+  mobileMenuBtn.addEventListener("click", function () {
+      // Toggle the visibility of the mobile dropdown
+      navLinks.style.display = (navLinks.style.display === "block") ? "none" : "block";
+  });
+});
